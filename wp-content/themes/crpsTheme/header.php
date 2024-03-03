65d3453778976d5126c2eb70664c348c467e9677
Ryan <39573308+RyanThen@users.noreply.github.com>
2024-03-03 18:47:48 -0500

    build main nav html structure

--- wp-content/themes/crpsTheme/header.php
+++ wp-content/themes/crpsTheme/header.php
@@ -11,8 +11,25 @@
 
   <header class="site-header">
     <div class="container">
+      <div class="site-header__content-container">
 
-    HEADER
+        <div class="site-header__logo-container">
+          <a href="#" class="site-header__logo-link">
+            <img class="site-header__logo" src="#">
+          </a>
+        </div>
 
+        <div class="site-header__menu-container">
+          <nav class="site-header__main-nav">
+            <ul class="site-header__menu-group">
+              <li class="site-header__menu-item menu-item"><a href="#" class="site-header__menu-item-link">One</a></li>
+              <li class="site-header__menu-item menu-item"><a href="#" class="site-header__menu-item-link">Two</a></li>
+              <li class="site-header__menu-item menu-item"><a href="#" class="site-header__menu-item-link">Three</a></li>
+              <li class="site-header__menu-item menu-item"><a href="#" class="site-header__menu-item-link">Four</a></li>
+            </ul>
+          </nav>
+        </div>
+
+      </div>
     </div>
   </header>
