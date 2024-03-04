9ed5150a00481b5088086733cf74c2f0486948af
Ryan <39573308+RyanThen@users.noreply.github.com>
2024-03-04 18:47:23 -0500

    setup sass file structure

--- wp-content/themes/crpsTheme/functions.php
+++ wp-content/themes/crpsTheme/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function crps_files() {
-  wp_enqueue_style('crps_styles', get_theme_file_uri('/build/index.css'));
+  wp_enqueue_style('crps_styles', get_theme_file_uri('/build/css/index.css'));
   wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
 }
